44dc382df87607f99483b9112e003d47d4eac231
JohanSing <johan.singainy-moutiapin@ynov.com>
2020-05-14 19:17:13 +0200

    Create an exception base with an exception which inherit to exception base for errors in sign in & sign up process

---
+++ src/exceptions/ExtendableErrorException.js
@@ -0,0 +1,10 @@
+class ExtendableErrorException extends Error {
+  constructor(message) {
+    super(message)
+    this.message = message
+    this.stack = new Error().stack
+    this.name = this.constructor.name
+  }
+}
+
+export default ExtendableErrorException
