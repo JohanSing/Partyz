f89dcea628e69ee18d64d4b7c35d4972405c915b
JohanSing <johan.singainy-moutiapin@ynov.com>
2020-05-14 19:19:34 +0200

    Add WelcomeScreen to router

--- src/config/router.js
+++ src/config/router.js
@@ -7,11 +7,13 @@ import {
 } from 'react-router-dom'
 
 import PrivateRoute from '../utils/PrivateRoute'
+import WelcomeScreen from '../screens/WelcomeScreen'
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
+        <Route exact path='/' component={WelcomeScreen} />
         <Redirect to='/home' />
       </Switch>
     </Router>
