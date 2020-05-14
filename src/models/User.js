c47a3815dfde18a7fd21f1829691ccaac7026818
JohanSing <johan.singainy-moutiapin@ynov.com>
2020-05-14 19:16:04 +0200

    Create a class User to build User object with response from sign in & sign up

---
+++ src/models/User.js
@@ -0,0 +1,26 @@
+class User {
+  constructor(id, email, role, authentication) {
+    this.id = id
+    this.email = email
+    this.role = role
+    this.authentication = authentication
+  }
+
+  getId() {
+    return this.id
+  }
+
+  getEmail() {
+    return this.email
+  }
+
+  getRole() {
+    return this.role
+  }
+
+  getAuthentication() {
+    return this.authentication
+  }
+}
+
+export default User
