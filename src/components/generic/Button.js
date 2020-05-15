e8a8956365256a6c18b1057d6522608e4ba5f067
JohanSing <johan.singainy-moutiapin@ynov.com>
2020-05-15 20:45:46 +0200

    Pending build header with homescreen

--- src/components/generic/Button.js
+++ src/components/generic/Button.js
@@ -5,14 +5,14 @@ import { theme } from '../../config/theme'
 export const Button = styled.button`
   background-color: ${props => props.bgColor ?? theme.colors['btn-primary']};
   color: ${props => props.fontColor ?? theme.colors['font-primary']};
-  font-size: 18px;
+  font-size: ${props => props.fontSize ?? 18}px;
   font-family: ${theme.fonts['font-primary']};
   width: ${props =>
     props.width
       ? theme.dimension.width - props.width
       : theme.dimension.width - 130}px;
   height: ${props => props.height ?? 100}px;
-  border-radius: 6px;
+  border-radius: ${props => props.borderRadius ?? 6}px;
   border: ${props => props.border ?? `none`};
   font-weight: 700;
   letter-spacing: 1px;
